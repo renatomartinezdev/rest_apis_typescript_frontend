434f8a31513a84561477a25fd149565ff187fa97
renatomartinezdev <renatom1912@gmail.com>
2025-07-14 21:28:32 -0300

    fix: removed unused addProduct import

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,6 +1,6 @@
 import { Link, Form, useActionData, type ActionFunctionArgs, redirect, type LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct, getProductsById, updateProduct } from "../services/ProductService";
+import {getProductsById, updateProduct } from "../services/ProductService";
 import type { Product } from "../types";
 import ProductForm from "../components/ProductForm";
 
